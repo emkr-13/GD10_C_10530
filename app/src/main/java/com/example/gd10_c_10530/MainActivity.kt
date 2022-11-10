158faa14191980235179c54937e7d246293887bd
emkr-13 <75230574+emkr-13@users.noreply.github.com>
2022-11-10 10:31:58 +0700

    awal

---
+++ app/src/main/java/com/example/gd10_c_10530/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.example.gd10_c_10530
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
