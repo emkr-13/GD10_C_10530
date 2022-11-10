467721f081b257593a9fa19e869ddaf5a6ecaa3c
emkr-13 <75230574+emkr-13@users.noreply.github.com>
2022-11-10 13:07:29 +0700

    update lagi

--- app/src/main/java/com/example/gd10_c_10530/RClient.kt
+++ app/src/main/java/com/example/gd10_c_10530/RClient.kt
@@ -4,7 +4,7 @@ import retrofit2.Retrofit
 import retrofit2.converter.gson.GsonConverterFactory
 
 object RClient {
-    private const val BASE_URL = "http://10.61.0.147/ci4-apiserver/public/"
+    private const val BASE_URL = "http://192.168.14.218/ci4-apiserver/public/"
     val instances:api by lazy {
         val retrofit = Retrofit.Builder()
             .baseUrl(BASE_URL)
